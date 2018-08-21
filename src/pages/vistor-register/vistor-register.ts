1eb0b07646b244f7f5f7c4f796b5d29f38e7d37e
tomwey <tomwey@163.com>
2018-08-21 12:26:12 +0800

    ccc

--- src/pages/vistor-register/vistor-register.ts
+++ src/pages/vistor-register/vistor-register.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { ApiService } from '../../provider/api-service';
+import { Tools } from '../../provider/Tools';
+import { Utils } from '../../provider/Utils';
+import { ResolvedReflectiveProvider_ } from '@angular/core/src/di/reflective_provider';
 
 /**
  * Generated class for the VistorRegisterPage page.
@@ -29,13 +33,69 @@ export class VistorRegisterPage {
   person: any = {
     custname: '',
     telephone: '',
+    knowway: '',
+    sex: '',
+    srctypename: '',
   };
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.person = this.navParams.data;
+  followtype: any;
+  constructor(public navCtrl: NavController, 
+    private api: ApiService,
+    private tools: Tools,
+    private modalCtrl: ModalController,
+    public navParams: NavParams) {
+    this.person = this.navParams.data.person;
+    this.person.followtype = this.navParams.data.followtype == 1 ? '来电' : '来访';
+
+    for (const key in this.person) {
+      if (this.person.hasOwnProperty(key)) {
+        if (this.person[key] == 'NULL') {
+          this.person[key] = '';
+        }
+      }
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VistorRegisterPage');
+  }
+
+  selectPersonSource() {
+    this.api.POST(null, { "dotype": "GetData", 
+        "funname": "通用获取数据字典数据APP", 
+        "param1": '415' })
+    .then(data => {
+      console.log(data);
+      if (data && data['data']) {
+        let arr = data['data'];
+        this.showSelectPage(arr);
+      }
+    })
+    .catch(error => {
+      this.tools.showToast(error.message || '获取项目失败');
+    });
+  }
+
+  showSelectPage(arr) {
+    let data = [];
+    // console.log(arr);
+    arr.forEach(element => {
+      // "project_id":"1291428","project_name":"珍宝金楠一期"
+      data.push(`${element.dic_name}|${element.dic_value}`);  
+    });
+    let selectedItem = null;
+    if (this.person.srctypename && this.person.srctypeid) {
+      selectedItem = `${this.person.srctypename}|${this.person.srctypeid}`;
+    }
+    let modal = this.modalCtrl.create('CommSelectPage', { selectedItem: selectedItem, 
+                                                          title: '选择来源', data: data })
+    modal.onDidDismiss((res) => {
+      if (res) {
+        // this.storage.set('selected.project', JSON.stringify(data));
+        this.person.srctypename = res.label;
+        this.person.srctypeid   = res.value;
+      }
+    });
+    modal.present();
   }
 
 }
--- src/pages/vistors-query/vistors-query.ts
+++ src/pages/vistors-query/vistors-query.ts
@@ -22,7 +22,7 @@ export class VistorsQueryPage {
 
   queryModel: any = {
     // proj_id: '1291564',
-    mobile: '',
+    mobile: '15000000000',
     name: '',
   };
 
@@ -30,6 +30,8 @@ export class VistorsQueryPage {
     label: '',
     value: ''
   };
+
+  followtype: any;
   
   dataList: any = [];
 
@@ -71,6 +73,10 @@ export class VistorsQueryPage {
 
   }
 
+  selectItem(person) {
+    this.forwardTo(person);
+  }
+
   showSelectPage(arr) {
     let data = [];
     // console.log(arr);
@@ -94,6 +100,8 @@ export class VistorsQueryPage {
   }
 
   query(type) {
+    this.followtype = type;
+
     this.api.POST(null, {
       dotype: 'GetData',
       funname: '案场查询客户访问记录APP',
@@ -107,19 +115,23 @@ export class VistorsQueryPage {
       let arr = data['data'];
       if (arr && arr.length > 0) {
         if (arr.length === 1) {
-          this.app.getRootNavs()[0].push('VistorRegisterPage', arr[0]);
+          this.forwardTo(arr[0]);
         } else {
           this.dataList = arr;
         }
       } else {
-        console.log('2222222');
-        this.app.getRootNavs()[0].push('VistorRegisterPage', { custname: this.queryModel, 
-                                                               telephone: this.queryModel.mobile });
+        // console.log('2222222');
+        this.forwardTo( { custname: this.queryModel.name, 
+          telephone: this.queryModel.mobile });
       }
     })
     .catch(error => {
       this.tools.showToast('查询失败，请重试~');
     });
+  }
+
+  forwardTo(person) {
+    this.app.getRootNavs()[0].push('VistorRegisterPage', { person: person, followtype: this.followtype });
   }
 
 }
