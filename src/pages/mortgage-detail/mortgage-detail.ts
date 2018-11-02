752f01ccec5566d484caae0eae5e060ed16ae50c
tomwey <tomwey@163.com>
2018-11-02 14:31:19 +0800

    ccc

--- src/pages/mortgage-detail/mortgage-detail.ts
+++ src/pages/mortgage-detail/mortgage-detail.ts
@@ -34,13 +34,13 @@ export class MortgageDetailPage {
     this.items.push({ label: '证件号码', value: this.mortgage.cardnos });
     this.items.push({ label: '贷款金额', value: this.mortgage.ajmoney });
     this.items.push({ label: '贷款方式', value: this.mortgage.paytypename });
-    this.items.push({ label: '按揭银行', value: this.mortgage.ajbank });
+    this.items.push({ label: '按揭银行', value: this.mortgage.ajbankname });
     this.items.push({ label: '贷款年限', value: this.mortgage.ajyears });
     this.items.push({ label: '按揭状态', value: this.mortgage.ajstate_desc });
     this.items.push({ label: '回款时间', value: this.mortgage.repaydate.replace('NULL', '--') });
     this.items.push({ label: '是否断供', value: parseInt(this.mortgage.isabort) == 0 ? '否' : '是'  });
-    this.items.push({ label: '异常类型', value: this.mortgage.ajstatedesc });
-    this.items.push({ label: '异常说明', value: this.mortgage.ajstatedesc });
+    this.items.push({ label: '异常类型', value: this.mortgage.abnormalname });
+    this.items.push({ label: '异常说明', value: this.mortgage.frontdesc });
     this.items.push({ label: '销售员', value: this.mortgage.sellername });
   }
 
