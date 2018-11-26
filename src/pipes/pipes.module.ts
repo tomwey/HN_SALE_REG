e56518c35ef49bc3ce09d66a538a3b683b14bb35
tomwey <tomwey@163.com>
2018-11-26 11:30:25 +0800

    ccc

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -3,15 +3,18 @@ import { TimeAgoPipe } from './time-ago/time-ago';
 import { FormatWanPipe } from './format-wan/format-wan';
 import { FormatNullPipe } from './format-null/format-null';
 import { FormatDatePipe } from './format-date/format-date';
+import { HackMobilePipe } from './hack-mobile/hack-mobile';
 @NgModule({
 	declarations: [TimeAgoPipe,
     FormatWanPipe,
     FormatNullPipe,
-    FormatDatePipe],
+    FormatDatePipe,
+    HackMobilePipe],
 	imports: [],
 	exports: [TimeAgoPipe,
     FormatWanPipe,
     FormatNullPipe,
-    FormatDatePipe]
+    FormatDatePipe,
+    HackMobilePipe]
 })
 export class PipesModule {}
