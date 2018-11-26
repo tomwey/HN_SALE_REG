eae4e7c9f98d71a54b0ce86bdb2ccc5a5b15ec86
tomwey <tomwey@163.com>
2018-11-26 13:46:49 +0800

    ccc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { TabsPage } from '../pages/tabs/tabs';
+// import { StatusBar } from '@ionic-native/status-bar';
+// import { SplashScreen } from '@ionic-native/splash-screen';
+// import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,8 +12,8 @@ export class MyApp {
 
   constructor(
     platform: Platform, 
-    statusBar: StatusBar, 
-    splashScreen: SplashScreen
+    // statusBar: StatusBar, 
+    // splashScreen: SplashScreen
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
