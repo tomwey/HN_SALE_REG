1c0213df5535689d9994215c61a1f92f50138746
tomwey <tomwey@163.com>
2018-12-24 10:08:25 +0800

    change some func

--- src/pages/mortgage-detail/mortgage-detail.ts
+++ src/pages/mortgage-detail/mortgage-detail.ts
@@ -21,7 +21,7 @@ export class MortgageDetailPage {
   items: any[] = [];
   mortgage: any = null;
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     private app: App,
     private events: Events,
     public navParams: NavParams) {
@@ -44,20 +44,20 @@ export class MortgageDetailPage {
     temp.push({ label: '客户名称', value: this.mortgage.custnames.replace('NULL', '--') });
     temp.push({ label: '房间号', value: this.mortgage.house_no.replace('NULL', '--') });
     temp.push({ label: '证件号码', value: this.mortgage.cardnos.replace('NULL', '--') });
-    temp.push({ label: '成交金额', value: `${this.mortgage.money.replace('NULL', '--')}万` });
-    temp.push({ label: '首付金额', value: `${this.mortgage.firstpay.replace('NULL', '--')}万` });
-    temp.push({ label: '贷款金额', value: `${this.mortgage.ajmoney.replace('NULL', '--')}万` });
+    temp.push({ label: '成交金额', value: `${this.mortgage.money.replace('NULL', '--')}` });
+    temp.push({ label: '首付金额', value: `${this.mortgage.firstpay.replace('NULL', '--')}` });
+    temp.push({ label: '贷款金额', value: `${this.mortgage.ajmoney.replace('NULL', '--')}` });
     temp.push({ label: '贷款方式', value: this.mortgage.paytypename.replace('NULL', '--') });
     temp.push({ label: '按揭银行', value: this.mortgage.ajbankname.replace('NULL', '--') });
     temp.push({ label: '贷款年限', value: this.mortgage.ajyears.replace('NULL', '--') });
     temp.push({ label: '按揭状态', value: this.mortgage.ajstate_desc.replace('NULL', '--') });
     temp.push({ label: '回款时间', value: this.mortgage.repaydate.replace('NULL', '--') });
-    temp.push({ label: '是否断供', value: parseInt(this.mortgage.isabort) == 0 ? '否' : '是'  });
-    
+    temp.push({ label: '是否断供', value: parseInt(this.mortgage.isabort) == 0 ? '否' : '是' });
+
     let exDesc = this.mortgage.abnormalname.replace('NULL', '--');
     if (exDesc !== '--') {
       temp.push({ label: '异常类型', value: this.mortgage.abnormalname.replace('NULL', '--') });
-      temp.push({ label: '异常说明', value:  this.mortgage.abnormaldesc.replace('NULL', '--')});
+      temp.push({ label: '异常说明', value: this.mortgage.abnormaldesc.replace('NULL', '--') });
     }
 
     temp.push({ label: '备注', value: this.mortgage.followupdesc.replace('NULL', '--') });
